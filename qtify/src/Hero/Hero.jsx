ef8f18780efc8a096e568e9da426898eed7a63ce
Subhasish Samaddar <subhasish.samaddar@techcrista.com>
2024-08-24 18:35:05 +0530

    modify text 2

--- qtify/src/Hero/Hero.jsx
+++ qtify/src/Hero/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
     <div className={styles.hero}>
       <div>
         <h1>100 Thousand Songs, ad-free</h1>
-        <h1>Over thousands podcast episodes 123</h1>
+        <h1>Over thousands podcast episodes</h1>
       </div>
       <div>
         <img
